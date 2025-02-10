1b88ad383cc3fe895492f9bfa51e9f6fee14a163
Hiroshi.tao <me@tcw.im>
2025-02-10 21:13:00 +0800

    fix db path opt

--- main.go
+++ main.go
@@ -48,13 +48,6 @@ func init() {
 	flag.UintVar(&port, "port", 7000, "http listen port")
 	flag.StringVar(&prefix, "prefix", "", "route prefix")
 	flag.StringVar(&dbpath, "db", "data/ip2region.xdb", "the ip2region.xdb filepath")
-
-	// 从 dbpath 加载 VectorIndex 缓存，把下述 vIndex 变量全局到内存里面。
-	var err error
-	vIndex, err = xdb.LoadVectorIndexFromFile(dbpath)
-	if err != nil {
-		panic(err)
-	}
 }
 
 func main() {
@@ -91,12 +84,19 @@ func startServer() {
 		port = uint(envport)
 	}
 	if stat, err := os.Stat(dbpath); err != nil || stat.IsDir() {
-		fmt.Println("invalid db")
+		fmt.Println("invalid db file")
 		return
 	}
 	if prefix != "" && !strings.HasPrefix(prefix, "/") {
 		fmt.Println("prefix must start with /")
 		return
+	}
+
+	// 从 dbpath 加载 VectorIndex 缓存，把下述 vIndex 变量全局到内存里面。
+	var err error
+	vIndex, err = xdb.LoadVectorIndexFromFile(dbpath)
+	if err != nil {
+		panic(err)
 	}
 
 	http.HandleFunc(routePattern("/myip"), ipView)
